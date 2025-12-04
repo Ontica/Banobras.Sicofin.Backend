54c2c764773c77427aaf3508272c7a7ceede49ec
José Manuel Cota <jmcota@ontica.org>
2025-12-04 11:34:08 -0500

    Rfx CalificationMoneda process and CALIFICA_MONEDA table were deprecated

--- ExternalInterfaces/BalancesExporter/Data/ExportBalancesDataService.cs
+++ ExternalInterfaces/BalancesExporter/Data/ExportBalancesDataService.cs
@@ -7,9 +7,7 @@
 *  Summary  : Data methods to store exported balances.                                                       *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
-using System;
 
-using Empiria.Collections;
 using Empiria.Data;
 
 using Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.Adapters;
@@ -20,17 +18,6 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.Data 
   static internal class ExportBalancesDataService {
 
     #region Public methods
-
-    static internal EmpiriaHashTable<CalificacionMoneda> GetCalificacionMonedaHashTable() {
-      var sql = "SELECT * FROM CALIFICA_MONEDA " +
-                "WHERE CALIFICA_SALDO IS NOT NULL";
-
-      var dataOperation = DataOperation.Parse(sql);
-
-      return DataReader.GetPlainObjectHashTable<CalificacionMoneda>(dataOperation,
-                                                                    x => $"{x.Cuenta}||{x.Sector}||{x.Auxiliar}");
-    }
-
 
     static internal void StoreBalances(ExportBalancesCommand command,
                                        FixedList<ExportedBalancesDto> balances) {
@@ -59,7 +46,6 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.Data 
 
     static private void WriteBalances(StoreBalancesInto storeInto,
                                       ExportedBalancesDto o) {
-      string nullString = null;
 
       var op = DataOperation.Parse("apd_saldo_tabla_intermedia",
                               storeInto.ToString(),
@@ -68,8 +54,7 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.Data 
                               o.Area, o.Moneda, o.NumeroMayor,
                               o.Cuenta, o.Sector, o.Auxiliar, o.FechaUltimoMovimiento,
                               o.Saldo, o.MonedaOrigen, o.NaturalezaCuenta, o.SaldoPromedio,
-                              o.MontoDebito, o.MontoCredito, o.SaldoAnterior,
-                              o.CalificaMoneda == "null" ? nullString : o.CalificaMoneda);
+                              o.MontoDebito, o.MontoCredito, o.SaldoAnterior, null);
 
       DataWriter.Execute(op);
     }
