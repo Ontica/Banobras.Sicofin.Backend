b20ead0dc07340895b12bb2a95083b0264c38ac4
efrain-jerv <efrain.rodriguez@ontica.org>
2026-04-21 18:32:46 -0700

    Functions to get balances to build CNBV64 and CNBV76 Report added

--- ExternalInterfaces/BalancesExporter/UseCases/ExportBalancesUseCases.cs
+++ ExternalInterfaces/BalancesExporter/UseCases/ExportBalancesUseCases.cs
@@ -8,14 +8,12 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
-
-using Empiria.Services;
-
+using Empiria.FinancialAccounting.BalanceEngine;
 using Empiria.FinancialAccounting.BalanceEngine.Adapters;
 using Empiria.FinancialAccounting.BalanceEngine.UseCases;
-
 using Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.Adapters;
 using Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.Data;
+using Empiria.Services;
 
 namespace Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.UseCases {
 
@@ -57,7 +55,67 @@ namespace Empiria.FinancialAccounting.BanobrasIntegration.BalancesExporter.UseCa
     }
 
 
+    public TrialBalanceDto GetBalanceForCNBV64Report(ExportBalancesCommand command) {
+      Assertion.Require(command, nameof(command));
+
+      using (var usecases = TrialBalanceUseCases.UseCaseInteractor()) {
+
+        TrialBalanceQuery _query = MapToBalanceQueryCNBV64(command);
+
+        return usecases.BuildTrialBalance(_query);
+      }
+    }
+
+
+    public TrialBalanceDto GetBalanceForCNBV76Report(ExportBalancesCommand command) {
+      Assertion.Require(command, nameof(command));
+
+      using (var usecases = TrialBalanceUseCases.UseCaseInteractor()) {
+
+        TrialBalanceQuery _query = MapToBalanceQueryCNBV76(command);
+
+        return usecases.BuildTrialBalance(_query);
+      }
+    }
+
     #endregion Use cases
+
+    #region Helpers
+
+    private TrialBalanceQuery MapToBalanceQueryCNBV64(ExportBalancesCommand command) {
+      return new TrialBalanceQuery {
+        TrialBalanceType = TrialBalanceType.Balanza,
+        AccountsChartUID = AccountsChart.IFRS.UID,
+        WithSubledgerAccount = true,
+        FromAccount = "1.15",
+        ToAccount = "1.15",
+        BalancesType = BalancesType.WithCurrentBalanceOrMovements,
+        ShowCascadeBalances = false,
+        InitialPeriod = {
+          FromDate = command.FromDate,
+          ToDate = command.ToDate
+        }
+      };
+    }
+
+
+    private TrialBalanceQuery MapToBalanceQueryCNBV76(ExportBalancesCommand command) {
+      return new TrialBalanceQuery {
+        TrialBalanceType = TrialBalanceType.Balanza,
+        AccountsChartUID = AccountsChart.IFRS.UID,
+        FromAccount = "20.01",
+        ToAccount = "20.01",
+        Ledgers = new string[] { "81816c16-3306-98b0-66bf-a69021e31171" },
+        BalancesType = BalancesType.AllAccounts,
+        ShowCascadeBalances = false,
+        InitialPeriod = {
+          FromDate = command.FromDate,
+          ToDate = command.ToDate
+        }
+      };
+    }
+
+    #endregion Helpers
 
   }  // class ExportBalancesUseCases
 
